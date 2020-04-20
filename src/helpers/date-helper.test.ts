0fcfe797417e538e6088f6787ea84f04e6040125
John Jiang <johnjiang101@gmail.com>
2020-04-19 21:17:10 -0700

    bug fix

--- src/helpers/date-helper.test.ts
+++ src/helpers/date-helper.test.ts
@@ -4,6 +4,7 @@ describe("date-helper", () => {
     describe("isInTimeRange", () => {
         const twoAm = new Date(2020, 3, 1, 12);
         const twelvePm = new Date(2020, 3, 1, 12, 5);
+        const ninePM = new Date(2020, 3, 1, 21, 5);
 
         it.each`
             time        | start | end   | expected
@@ -11,22 +12,25 @@ describe("date-helper", () => {
             ${twelvePm} | ${4}  | ${12} | ${false}
             ${twelvePm} | ${4}  | ${9}  | ${false}
             ${twoAm}    | ${23} | ${3}  | ${false}
+            ${ninePM}   | ${21} | ${4}  | ${true}
         `("returns expected", ({ time, start, end, expected }) => {
             expect(isInTimeRange(time, { start, end })).toBe(expected);
         });
     });
 
     describe("isInMonthRange", () => {
-        const december = new Date(2020, 11, 1);
         const january = new Date(2020, 0, 1);
         const april = new Date(2020, 3, 1);
+        const november = new Date(2020, 10, 1);
+        const december = new Date(2020, 11, 1);
 
         it.each`
             time        | start | end   | expected
-            ${december} | ${9}  | ${11} | ${true}
-            ${december} | ${4}  | ${9}  | ${false}
             ${january}  | ${11} | ${3}  | ${true}
             ${april}    | ${4}  | ${9}  | ${false}
+            ${december} | ${9}  | ${11} | ${true}
+            ${december} | ${4}  | ${9}  | ${false}
+            ${december} | ${10} | ${3}  | ${true}
         `("returns expected", ({ time, start, end, expected }) => {
             expect(isInMonthRange(time, { start, end })).toBe(expected);
         });
--- src/helpers/date-helper.ts
+++ src/helpers/date-helper.ts
@@ -3,10 +3,14 @@ import { MonthRange, TimeRange } from "../pages/fish-guide/fish-table";
 
 export function isInTimeRange(time: Date, interval: TimeRange): boolean {
     let start = set(time, { hours: interval.start, minutes: 0 });
-    const end = set(time, { hours: interval.end, minutes: 0 });
+    let end = set(time, { hours: interval.end, minutes: 0 });
     // e.g. start: 9pm end: 4am, so we need to set start to be the day before
     if (interval.end < interval.start) {
-        start = addDays(start, -1);
+        if (time.getHours() >= interval.start) {
+            end = addDays(end, 1);
+        } else {
+            start = addDays(start, -1);
+        }
     }
 
     return isWithinInterval(time, {
@@ -17,10 +21,14 @@ export function isInTimeRange(time: Date, interval: TimeRange): boolean {
 
 export function isInMonthRange(time: Date, interval: MonthRange): boolean {
     let start = setMonth(time, interval.start);
-    const end = setMonth(time, interval.end);
+    let end = setMonth(time, interval.end);
     // e.g. start: Dec end: Feb, so we need to set start to be the year before
     if (interval.end < interval.start) {
-        start = addYears(start, -1);
+        if (time.getMonth() >= interval.start) {
+            end = addYears(end, 1);
+        } else {
+            start = addYears(start, -1);
+        }
     }
 
     return isWithinInterval(time, {
